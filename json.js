ded85b8a375f220a0399f2181d47988d71752a1e
coderdecember29 <96750361+coderdecember29@users.noreply.github.com>
2022-10-05 20:32:21 -0400

    Add files via upload

---
+++ json.js
@@ -0,0 +1,42 @@
+var prediction1 = "";
+var prediction2 = "";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takesnapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">';
+    });
+}
+
+
+console.log("ml5version",ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ozC5IvI4x/model.json',moduelloaded);
+
+function moduelloaded()
+{
+    console.log("moduelloaded");
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "first prediction is" + prediction1;
+    speak_data_2 = "and the second prediction is " + prediction2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+
+
+
